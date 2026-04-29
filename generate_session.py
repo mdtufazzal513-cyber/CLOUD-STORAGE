356487a54a6110f36859a0c3b8d07ee55ac6268a
mdtufazzal513-cyber <mdtufazzal513@gmail.com>
2026-04-29 19:07:41 +0600

    Change BOT_TOKEN in generate_session.py
    Updated BOT_TOKEN for the bot session.

--- generate_session.py
+++ generate_session.py
@@ -3,7 +3,7 @@ import asyncio
 
 API_ID = 33445387 # আপনার API ID
 API_HASH = "5b1badf6d0f44c940a2263cef28d6689"
-BOT_TOKEN = "8781052287:AAEYTaE5Cj1sR4dokfsdhlTKXg1t5Kgejd0"
+BOT_TOKEN = "8781052287:AAFi5-uN_mBRZKjeeA0ije0E2mlUrdTQJ0s"
 
 async def main():
     bot = Client("my_bot_session", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True)
